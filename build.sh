c2ba4c07e240f8967e6b9102f32698d00fd16eb8
krishnaspeace <krishnasaarav@gmailcom>
2024-03-28 02:25:41 +0530

    init evo

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b crdroid .repo/local_manifests
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b evo .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
 # Clone crdroid 14
-repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b uqpr2
 # repo sync
 /opt/crave/resync.sh
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
-export CRDROID_MAINTAINER=krishnaspeace
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 # Lunch configuration
-lunch lineage_begonia-userdebug
+lunch evolution_begonia-userdebug
 # Build confriguration
-m bacon
+m evolution
