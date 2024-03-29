a88d952ffdc72d2e8ac67a7068b0f8c4d6509cdd
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-03-29 19:10:35 +0530

    Shift local manifests

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
+# Clone evo
+repo init -u https://github.com/Evolution-X/manifest -b uqpr2
+# repo sync
+/opt/crave/resync.sh
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b evo-bego .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
-# Clone crdroid 14
-repo init -u https://github.com/Evolution-X/manifest -b uqpr2
-# repo sync
-/opt/crave/resync.sh
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
