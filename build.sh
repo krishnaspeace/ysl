6c60e3e8423f58c1b4d348e389d4368b44a704e0
krishnaspeace <krishnasaarav@gmailcom>
2024-04-17 19:04:04 +0530

    y

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-# Clone Evolution X
-repo init -u https://github.com/Evolution-X/manifest -b u
-# repo sync
-/opt/crave/resync.sh
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b evo-bego .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
+# Clone Evolution X
+repo init -u https://github.com/Evolution-X/manifest -b u
+# repo sync
+/opt/crave/resync.sh
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
