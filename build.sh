8a9e5ca3d90ded96898117d765ba1e251b761939
krishnaspeace <krishnasaarav@gmailcom>
2024-04-18 16:22:29 +0530

    improve script for resolving error

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+rm -rf .repo/local_manifests/ 
+# Clone Evolution X
+repo init -u https://github.com/Evolution-X/manifest -b u
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b evo-bego .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
-# Clone Evolution X
-repo init -u https://github.com/Evolution-X/manifest -b u
 # repo sync
 /opt/crave/resync.sh
 # Set up build environment
