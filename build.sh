8215879cbffdac290a4a01e11114f8cbaf397b6a
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-05-16 17:44:08 +0530

    script: fix trees sync issue

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Clone crdroid 14
 repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-# repo sync
-/opt/crave/resync.sh
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
+# repo sync
+/opt/crave/resync.sh
 # Fixing fingerprint
 rm -rf vendor/fingerprint/opensource/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
