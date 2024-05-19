4b6e39ec9670e02b6e805b1d637862f4c17009bd
krishnaspeace <krishnasaarav@gmailcom>
2024-05-19 19:01:35 +0530

    script: pixy

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -rf .repo/local_manifests/ 
-# Clone Bilss Os
-repo init -u https://github.com/BlissRoms/platform_manifest.git -b universe --git-lfs
+# Clone Pixy Os
+repo init -u https://github.com/PixysOS/manifest -b fourteen-v2
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b bliss .repo/local_manifests
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b pixy .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
@@ -11,7 +11,9 @@ if [ ! 0 == 0 ]
 # Set up build environment
 export BUILD_USERNAME=kr
 export BUILD_HOSTNAME=crave
-export BLISS_BUILD_VARIANT=gapps
+export PIXYS_MAINTANER=KRISHNA
 source build/envsetup.sh
-# running configuration
-bliss -g begonia
+# lunch configuration
+lunch pixys_begonia-ap1a-userdebug
+# let's build it
+m pixysrelease 
