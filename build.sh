83580ebec77a99b661cc88bace4ee1e9e7a237ac
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-06-02 19:33:56 +0530

    script: removing init part

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 rm -rf .repo/local_manifests/
-# Clone Bilss Os
-repo init -u https://github.com/BlissRoms/platform_manifest.git -b universe --git-lfs
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b bliss .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
  fi
-# fix crave error
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 # repo sync
 /opt/crave/resync.sh
 # Fix git-lfs
