7f6d35b50636ecdb7aa8343681e8cd0613a0c6e6
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2024-06-05 21:31:23 +0530

    script: fix sync issue

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 # repo sync
-/opt/crave/resync.sh
+curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
 # Fix git-lfs
 repo forall -c "git lfs install && git lfs pull && git lfs checkout" 
 # Set up build environment
